9802f950ccd60e523cbec88e29fbbb23e1ac2cf7
harrisuddin <harris7001@gmail.com>
2020-01-15 17:41:47 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// Dependencies
+var http = require('http');
+var request = require('request');
+var express = require('express');
+
+var app = express();
+const redirect_uri = 'https://td-api-medium.herokuapp.com/auth';
