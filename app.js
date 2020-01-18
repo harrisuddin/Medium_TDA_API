78b58b5d054771f6d6689ce083ed9695b956420b
harrisuddin <harris7001@gmail.com>
2020-01-18 16:18:57 +0000

    implemented autologin

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var http = require('http');
 var request = require('request');
 var express = require('express');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 var app = express();
 const redirect_uri = 'https://td-api-medium.herokuapp.com/auth';
@@ -35,14 +37,55 @@ app.get('/auth', (req, res) => {
             authReply = JSON.parse(body);
             // to check it's correct, display it
             res.send(authReply);
-        } else {
-            // if there are errors, display them with an error 400
-            res.status(400).json({error: error});
         }
 
     });
 
 });
+
+async function autoLogin() {
+
+    var details;
+    await fs.readFile('details.json', (err, data) => {
+        if (err) console.error(err);
+        details = JSON.parse(data);
+        console.log(details);
+    });
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=${encodeURI(redirect_uri)}&client_id=${process.env.CLIENT_ID}%40AMER.OAUTHAP`);
+
+    // Enter username
+    await page.click('#username');
+    await page.keyboard.type("TrueAlpha");
+
+    // Enter password
+    await page.click('#password');
+    await page.keyboard.type("dailyvolx16");
+
+    // Click login button
+    await page.click('#accept');
+
+    // Click allow button
+    await page.click('#accept');
+
+    var updatedTimes = {
+        access_last_update: Date().toString(),
+        refresh_last_update: Date().toString()
+    };
+
+    await fs.writeFile('details.json', JSON.stringify(updatedTimes, null, 2), (err) => {
+        if (err) console.error(err);
+    }); 
+
+    // Test page is correct with screenshot
+    //await page.screenshot({path: 'example.png'});
+
+    // Close browser
+    await browser.close();
+
+}
 
 // start server
 var httpServer = http.createServer(app);
